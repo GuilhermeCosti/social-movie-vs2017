9910486eff4d4b82fb5d2c943644206a7867e4da
joaopedropio <joao-pio@hotmail.com>
2017-07-21 10:57:23 -0300

    https working

--- SocialMovie/wwwroot/js/site.js
+++ SocialMovie/wwwroot/js/site.js
@@ -1,6 +1,6 @@
 ﻿// setup the video element and attach it to the Dash player
 function setupVideo() {
-    var url = "movies/stream.mpd";
+    var url = "https://179.228.89.153:9443/split/video/stream.mpd";
     var player = dashjs.MediaPlayer().create();
     player.initialize(document.querySelector("#videoPlayer"), url, true);
 }
