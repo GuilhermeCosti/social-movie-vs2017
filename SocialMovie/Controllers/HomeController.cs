31b1cfb99a77e69eb3ce5b01150720fc70d03fdb
joaopedropio <joao-pio@hotmail.com>
2017-08-14 22:41:07 -0300

    changeg soma front end

--- SocialMovie/Controllers/HomeController.cs
+++ SocialMovie/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SocialMovie.Models;
+using Microsoft.AspNetCore.Authorization;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,10 +20,11 @@ namespace SocialMovie.Controllers
             _context.Database.EnsureCreated();
         }
 
+        [Authorize]
         public IActionResult Index()
         {
-            return View();
-            //return RedirectToAction("Login", "Account");
+            //return View();
+            return RedirectToAction("Index", "MainMenu");
         }
     }
 }
