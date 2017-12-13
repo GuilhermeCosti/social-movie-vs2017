7021af630294d834f94ea83d77292ab7d6f3bd4e
Joao Pedro <joao-pio@hotmail.com>
2017-12-13 17:21:25 +0000

    modified build script

--- SocialMovie/build.sh
+++ SocialMovie/build.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 docker rm -f socialmovie
-docker rmi socialmovie
-docker build -t socialmovie .
-docker run -d --network=local-network -p 5000:80 --name socialmovie socialmovie
+#docker rmi socialmovie
+#docker build -f Dockerfile -t socialmovie .
+#docker run -d --network=local-network -p 5000:80 --name socialmovie socialmovie
 
