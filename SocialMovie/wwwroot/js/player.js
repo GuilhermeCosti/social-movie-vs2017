568aae533d11117bdcb77b0c415432196cdab831
Joao Pedro <joao-pio@hotmail.com>
2017-12-29 21:05:38 -0200

    Modifeid dash script and authorization header add to it

--- SocialMovie/wwwroot/js/player.js
+++ SocialMovie/wwwroot/js/player.js
@@ -2,5 +2,15 @@
 function setupVideo(protocol, domain, port, path, file, ext, id) {
     var url = protocol + "://" + domain + ":" + port + "/" + path + "/" + file + "." + ext;
     var player = dashjs.MediaPlayer().create();
+    player.extend("RequestModifier", () => {
+        return {
+            modifyRequestHeader: xhr => {
+                xhr.setRequestHeader("auth", "123asfasf4");
+                return xhr;
+            }
+        };
+    },
+        true
+    );
     player.initialize(document.querySelector(`#video_${id}`), url, true);
 }
